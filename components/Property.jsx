679fd09d25e3fc0f92a958e42264f15f051d8c92
Steven <gonghaima@hotmail.com>
2021-12-30 21:49:48 +1100

    add property details

--- components/Property.jsx
+++ components/Property.jsx
@@ -18,8 +18,39 @@ const Property = ({
     isVerified,
     externalId,
   },
-}) => {
-  return <Link href={`/property/${externalId}`} passHref>{title}</Link>;
-};
+}) => (
+  <Link href={`/property/${externalId}`} passHref>
+    <Flex
+      flexWrap="wrap"
+      w="420px"
+      p="5"
+      paddingTop="0"
+      justifyContent="flex-start"
+      cursor="pointer"
+    >
+      <Box>
+        <Image
+          src={coverPhoto ? coverPhoto.url : DefaultImage}
+          width={400}
+          height={260}
+          alt="house"
+        />
+      </Box>
+      <Box w="full">
+        <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+          <Flex alignItems="center">
+            <Box paddingRight="3" color="green.400">
+              {isVerified && <GoVerified />}
+            </Box>
+            <Text fontWeight="bold" fontSize="lg">
+              AED {millify(price)}
+              {rentFrequency && `/${rentFrequency}`}
+            </Text>
+          </Flex>
+        </Flex>
+      </Box>
+    </Flex>
+  </Link>
+);
 
 export default Property;
